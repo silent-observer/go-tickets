fb9e8920ac8563d2856e946c8cc936d1216d66e3
Maksim Michkov <m.michkov@arenadata.io>
2025-11-01 00:00:00 +0300

    Move protobuf outputs

--- gen_proto.bash
+++ gen_proto.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 pushd proto
-protoc --proto_path=. --go_out=../proto_go --go_opt=paths=source_relative *.proto
+protoc --proto_path=. --go_out=. --go_opt=paths=source_relative \
+ --go-grpc_out=. --go-grpc_opt=paths=source_relative *.proto
 popd
---
+++ proto/go.mod
@@ -0,0 +1,3 @@
+module github.com/silent-observer/go-tickets/proto
+
+go 1.25.3
